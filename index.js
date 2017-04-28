7d7e85124aff4cb8d08017ad53b86daa6f35fdc8
Mikhail Faraponov <mikefaraponov@users.noreply.github.com>
2017-04-28 08:44:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,90 @@
-const cache = Symbol(':cache');
+const globby = require('globby');
+const path = require('path');
 
 /**
- * Apok Dependency Injection Container
+ * Apok
  */
 class Apok {
   /**
    * Constructor for dependency injection container
-   * @constructor
+   * @param  {String} dirname mount point
    */
-  constructor() {
-    this[cache] = new Map();
+  constructor(dirname) {
+    this._dirname = dirname;
+    this._cache = new Map();
+    this._patterns = [];
+    this._methodNamePattern = /(lazyton|lazy|factory|singleton)(.*)/;
+    this._context = {};
   }
   /**
-   * Returns executed method for some interface method
-   * that should be implmented in derived containers classes
-   * @param  {String} bean some string that describes implemented bean factory
-   * @return {Object} instanciated class
+   * Match file name pattern (lookup path)
+   * @param  {String} pattern [description]
+   * @return {Apok} this context
    */
-  getBean(bean) {
-    return this[bean]();
+  match(pattern) {
+    this._patterns.push(pattern);
+    return this;
   }
   /**
-   * Lazy object initialization helper
-   * @param  {Function} constructor code block for bean creation
-   * @return {Proxy} proxy for lazy created object
+   * Build container
+   * @return {Apok} builded container with context
+   */
+  build() {
+    globby
+      .sync(this._patterns.map(this._join.bind(this)))
+      .map(require.bind(require))
+      .reduce(this._inject.bind(this), this._context);
+    return this;
+  }
+  /**
+   * Join full path with pattern
+   * @param  {String} pattern globby pattern
+   * @return {String} full path pattern
+   */
+  _join(pattern) {
+    return path.join(this._dirname, pattern);
+  }
+  /**
+   * Injects constructors to context
+   * @return {Object} context
+   */
+  _inject(context, clazz) {
+    return Object.getOwnPropertyNames(clazz)
+      .filter((factoryName) => this._methodNamePattern.test(factoryName))
+      .map((factoryName) => this._methodNamePattern.exec(factoryName))
+      .reduce((context, [entire, scope, className]) => {
+        const self = this;
+        let instanciate = () => clazz[entire](context);
+        return Object.assign(context, {
+          [className]() {
+            if (scope === 'singleton') {
+              return self.singleton(className, instanciate);
+            } else if (scope === 'factory') {
+              return instanciate();
+            } else if (scope === 'lazy') {
+              return self.lazy(instanciate);
+            } else if (scope === 'lazyton') {
+              return self.singleton(className, () => self.lazy(instanciate));
+            } else {
+              throw new TypeError('Injection failed!');
+            }
+          },
+        });
+      }, context);
+  }
+  /**
+   * Lazy initializer
+   * @param  {Function} constructor constructor function
+   * @return {Proxy} proxy for initialization object
    */
   lazy(constructor) {
     let instance = null;
     return new Proxy({}, {
-      get(target, prop) {
+      get(target, property) {
         if (!instance) {
           instance = constructor();
         }
-        return instance[prop];
+        return instance[property];
       }
     });
   }
@@ -43,11 +95,38 @@ class Apok {
    * @return {Object} returns new or cached value
    */
   singleton(bean, constructor) {
-    if (this[cache].has(bean)) {
-      this[cache].set(bean, constructor());
+    if (!this._cache.has(bean)) {
+      this._cache.set(bean, constructor());
     }
-    return this[cache].get(bean);
+    return this._cache.get(bean);
+  }
+  /**
+   * Prototype for mocking (e.g. sinon, mocha)
+   * @return {Object} context
+   */
+  get prototype() {
+    return this._context;
+  }
+  /**
+   * Setter for prototype cannot be set!!!
+   * @throws {TypeError} If setter called error thrown
+   */
+  set prototype() {
+    throw new TypeError('Prototype cannot be replaced!!!');
+  }
+  /**
+   * API method to create bean from context
+   * @return {Object} bean instance
+   */
+  getBean(bean) {
+    return this._context[bean]();
+  }
+  /**
+   * Static factory
+   * @param  {Srging} dirname mount point
+   * @return {Apok} new container
+   */
+  static of(dirname) {
+    return new Apok(dirname);
   }
 }
-
-module.exports = Apok;
